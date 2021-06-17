63f89521c7c22009302c20ae60a05f0b5e0a21dc
Francisco Aldado <81989283+faldado82@users.noreply.github.com>
2021-06-17 16:54:42 -0300

    Add files via upload
    ##### NUEVO en V3.2 ######
    - Sección 4 Alumno (Checked Ok)
    - Cambios menores en nombres de usuarios.
    - Corregidos problemas por funcionalidad al agregar Array entregasIndividuales en Clase Alumno
    
    ##### ANTERIORMENTE en V3.1 #####
    - Agregada imagen a tabla Sección 3
    - Sección 3 Alumno (Checked Ok)
    - Quitado cantidadEntregas de Clase Alumno. En su lugar se coloco Array entregasIndividuales para el uso en Sección 4.
    - Sección 4 Docente (Checked Ok)
    - Correcciones menores para ser mas amigable en los mensajes para el usuario.
    
    ##### ANTERIORMENTE en V3.0 #####
    - Sección 3 Alumno (sin finalizar, falta incluir imágenes)
    - Se agrego cantidadEntregas a Clase Alumno para testing de Sección 4 Docente
    - Se comenzó a desarrollar Sección 4 Docente (sin finalizar)
    - Cambios menores en los datos de precargas Docentes.
    
    ##### ANTERIORMENTE en V2.9 #####
    - Funcionalidad Sección 3 Docente (Checked Ok) 
    
    ##### ANTERIORMENTE en V2.7 #####
    - Funcionalidad Sección 1 y Sección 2 (Checked OK en Docente y Alumno) 
    - Se quito elemento "estado" de clase Entrega
    - Funcionalidad "Log Out" Activa en Docente y Alumno
    - Se agrego preCargaEntregas. Dos elementos a Array Objeto "entregas" para pruebas para Sección 3 Docente
    - Se comenzó a desarrollar Sección 3 Docente (Sin Finalizar)
    
    ##### ANTERIORMENTE en V2.6 #####
    - Se implemento funcionalidad de Búsqueda de Seccion1 Alumno (Sin finalizar)
    
    ##### ANTERIORMENTE en V2.5 ########
    - Se agrego nueva clase Objeto "entregas"
    - Se modificaron passwords y nombres de usuarios
    - Se agrego Precarga de Ejercicios
    - Sección 1 y 2 de Docente Checked sin errores
    - Se agrego funcionalidad de sección 1 para Alumno "Mostrar Ejercicios" (Sin terminar Seccion1 - Alumno)
    
    #### ANTERIORMENTE en V2.3 ####
    Modificaciones:
    - Se unieron Docentes y Alumnos a solo Usuarios (en un solo Array de Objetos)
    - Se Modificaron nombres de Usuarios
    - Se implemento Navegación según usuario
    - Se agrego funcionalidad para ver seccion1 de Docente
    
    ##### ANTERIORMENTE en V2.1 #####
    Precargas:
    - Niveles
    - Docentes
    - Alumnos
    
    UI:
    - Pantalla Bienvenida
    - Funcionalidad Login (Alumno y Docente)
    - Funcionalidad Registro (Alumno y Docente)
    
    Validaciones:
    - Password
    - Nombre
    - Espacios en medio de Password

---
+++ Obligatorio V3.2/js/clases.js
@@ -0,0 +1,56 @@
+// Clase para crear Nivel
+class Nivel {
+  constructor() {
+    this.numero; // number
+    this.nombre; // string
+  }
+}
+
+// Clase para crear Docente
+class Docente {
+  constructor() {
+    this.nombre; // string
+    this.usuario; // string
+    this.password; // string
+    this.tipo = 'docente';
+  }
+}
+
+// Clase para crear Alumno
+class Alumno {
+  constructor() {
+    this.nombre; // string
+    this.usuario; // string
+    this.password; // string
+    this.Nivel; // objeto
+    this.Docente; // objeto
+    this.tipo = 'alumno';
+    this.entregasIndividuales = [];
+  }
+}
+
+// Clase para crear Ejercicio
+class Ejercicio {
+  static nro = 1;
+  constructor() {
+    this.Nivel; // objeto
+    this.id = Ejercicio.nro++; // identificador unico
+    this.tituloEjercicio; // string
+    this.descripcionEjercicio; // string
+    this.imagen; // image file
+    this.Docente;
+  }
+}
+
+// Clase para crear Entrega
+class Entrega {
+  static nro = 1;
+  constructor() {
+    this.id = Entrega.nro++; // identificador unico
+    this.Alumno; // objeto
+    this.Ejercicio; // objeto
+    this.Docente; // objeto
+    this.sonido; // audio file
+    this.devolucion; // string
+  }
+}
